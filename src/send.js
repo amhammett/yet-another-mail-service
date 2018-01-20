ab397fb52b5dab2af22741b1b117968b8c70d949
Andrew Hammett <hammett@amh.io>
2018-01-20 12:50:28 -0800

    start using smtp nodemailer plugin. simplify logic

--- src/send.js
+++ src/send.js
@@ -1,51 +1,39 @@
 'use strict';
 
-const smtp_host = process.env.SMTP_HOST || 'mail.host.com'
-const smtp_port = process.env.SMTP_PORT || 587
-
-console.log('using: '+smtp_host)
-
-let send_email = (message) => {
-  console.log('send message')
-  console.log(message)
-
-  let nodemailer = require('nodemailer');
-  let transporter = nodemailer.createTransport({
-      host: smtp_host,
-      port: smtp_port,
-      secure: false
-  });
-
-  transporter.verify(function(error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      transporter.sendMail(message, function(error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Message sent: '+ info.response);
-        }
-      });
-    }
-  });
-}
-
 module.exports.send = (event, context, callback) => {
-  if(event.Records) {
-    event.Records.forEach(function(record) {
-      let record_data = new Buffer((record.kinesis.data), 'base64').toString("ascii");
-      console.log(record_data)
-      let kinesis_data = JSON.parse(record_data)
-      //let event_data = JSON.parse(kinesis_data.body)
-      send_email(kinesis_data)
-    });
-  }
+  console.log('event')
+  console.log(event)
+  const nodemailer = require('nodemailer');
+  let smtpTransport = require('nodemailer-smtp-transport');
 
-  callback(null, {
-    statusCode: 200,
-    headers: { 'Content-Type': 'text/plain' },
-    body: 'Greatness awaits.'
+  event.Records.forEach(function(record) {
+    console.log('record')
+    console.log(record)
+    console.log('processing mail record')
+
+    let transporter = nodemailer.createTransport(
+      smtpTransport({
+        host: process.env.SMTP_HOST || 'mail.host.com',
+        port: process.env.SMTP_PORT || 587,
+        secure: false
+      })
+    );
+
+    let record_data = new Buffer((record.kinesis.data), 'base64').toString("ascii");
+    let message = JSON.parse(record_data)
+    console.log(message)
+
+    transporter.sendMail(message, function(error, info) {
+      if (error) {
+        console.log(error);
+        context.fail(error, 'failed')
+      } else {
+        console.log('Message sent: '+ info.response);
+        context.succeed(null, 'Completed')
+      }
+      transporter.close()
+    });
   });
-  return;
+
+  callback(null, 'Greatness awaits.');
 }
